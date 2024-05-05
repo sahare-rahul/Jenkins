bfcf39f220e60759a43c099ea3c8ccdf02219d14
sahare-rahul <151616061+sahare-rahul@users.noreply.github.com>
2024-05-05 19:51:16 +0530

    Update resource.groovy

--- vars/resource.groovy
+++ vars/resource.groovy
@@ -1,3 +1,7 @@
 # we are calling script of Resource folder  in pipeline by using "vars" 
 
-deff
+def call() {
+  def content LibraryResource "bash.sh"   #bash.sh is file name form resource folder , here LibraryREsource extracting content from bash.sh and storing in content 
+  writeFile file: "script.sh" , text: content        #and in next step writeFile storing this content in script.sh file and creat thos file 
+  sh "sh script.sh"
+}
